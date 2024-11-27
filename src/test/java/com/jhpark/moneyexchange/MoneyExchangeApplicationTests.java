41245fa3fa55a1498b1d797ead2289605b08e7ac
Park JaeHong <blogerhong@naver.com>
2024-11-27 19:41:36 +0900

    add : Init new project

---
+++ src/test/java/com/jhpark/moneyexchange/MoneyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhpark.moneyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
